d960082b0868d29bc720cc492c0bb7cc50fe0ce2
aditya-kapoor <aditya.kapoor@vinsol.com>
2014-01-01 21:33:45 +0530

    moving some config options to the configuration file

--- gems/fullcalendar-rails-engine/config/initializers/configuration.rb
+++ gems/fullcalendar-rails-engine/config/initializers/configuration.rb
@@ -1,15 +1,17 @@
 config = File.exists?('config/fullcalendar.yml') ? YAML.load_file("config/fullcalendar.yml") : {}
 FullcalendarEngine::Configuration = {
-  'editable' => true,
-  'header' => {
-            left: 'prev,next today',
-            center: 'title',
-            right: 'month,agendaWeek,agendaDay'
-          },
+  'editable'    => true,
+  'header'      => {
+    left: 'prev,next today',
+    center: 'title',
+    right: 'month,agendaWeek,agendaDay'
+  },
   'defaultView' => 'agendaWeek',
-  'height' => 500,
+  'height'      => 500,
   'slotMinutes' => 15,
   'dragOpacity' => 0.5,
-  'selectable' => true
+  'selectable'  => true,
+  'events'      => "fullcalendar_engine/events/get_events",
+  'timeFormat'  => "h:mm t{ - h:mm t}"
 }
 FullcalendarEngine::Configuration.merge!(config)
