eb7f530fa9d835e0655e7e3d7cde1cfb8e6cb0fd
aditya-kapoor <aditya.kapoor@vinsol.com>
2014-01-01 21:19:53 +0530

    made some cosmetic changes to events.js

--- gems/fullcalendar-rails-engine/app/assets/javascripts/fullcalendar_engine/events.js
+++ gems/fullcalendar-rails-engine/app/assets/javascripts/fullcalendar_engine/events.js
@@ -18,6 +18,8 @@ FullcalendarEngine.Form = {
 
     $('#event_all_day').attr('checked', options['allDay'])
 
+    // FullcalendarEngine.Form.fetch()
+
     $('#create_event_dialog').dialog({
       title: 'New Event',
       modal: true,
@@ -32,55 +34,67 @@ FullcalendarEngine.Form = {
     $day.prop('selectedIndex', time.getDate() - 1);
     $hour.prop('selectedIndex', time.getHours());
     $minute.prop('selectedIndex', time.getMinutes());
+  },
+  fetch: function(){
+    jQuery.ajax({
+      type: 'get',
+      dataType: 'script',
+      async: true,
+      url: "/fullcalendar_engine/events/new"
+    });
   }
 }
 
 function moveEvent(event, dayDelta, minuteDelta, allDay){
-    jQuery.ajax({
-        data: 'id=' + event.id + '&title=' + event.title + '&day_delta=' + dayDelta + '&minute_delta=' + minuteDelta + '&all_day=' + allDay + '&authenticity_token=' + authenticity_token,
-        dataType: 'script',
-        type: 'post',
-        url: "fullcalendar_engine/events/move"
-    });
+  jQuery.ajax({
+    data: 'id=' + event.id + '&title=' + event.title + '&day_delta=' + dayDelta + '&minute_delta=' + minuteDelta + '&all_day=' + allDay + '&authenticity_token=' + authenticity_token,
+    dataType: 'script',
+    type: 'post',
+    url: "/fullcalendar_engine/events/move"
+  });
 }
 
 function resizeEvent(event, dayDelta, minuteDelta){
-    jQuery.ajax({
-        data: 'id=' + event.id + '&title=' + event.title + '&day_delta=' + dayDelta + '&minute_delta=' + minuteDelta + '&authenticity_token=' + authenticity_token,
-        dataType: 'script',
-        type: 'post',
-        url: "fullcalendar_engine/events/resize"
-    });
+  jQuery.ajax({
+    data: 'id=' + event.id + '&title=' + event.title + '&day_delta=' + dayDelta + '&minute_delta=' + minuteDelta + '&authenticity_token=' + authenticity_token,
+    dataType: 'script',
+    type: 'post',
+    url: "/fullcalendar_engine/events/resize"
+  });
 }
 
 function showEventDetails(event){
-    $('#event_desc').html(event.description);
-    $('#edit_event').html("<a href = 'javascript:void(0);' onclick ='editEvent(" + event.id + ")'>Edit</a>");
-    if (event.recurring) {
-        title = event.title + "(Recurring)";
-        $('#delete_event').html("&nbsp; <a href = 'javascript:void(0);' onclick ='deleteEvent(" + event.id + ", " + false + ")'>Delete Only This Occurrence</a>");
-        $('#delete_event').append("&nbsp;&nbsp; <a href = 'javascript:void(0);' onclick ='deleteEvent(" + event.id + ", " + true + ")'>Delete All In Series</a>")
-        $('#delete_event').append("&nbsp;&nbsp; <a href = 'javascript:void(0);' onclick ='deleteEvent(" + event.id + ", \"future\")'>Delete All Future Events</a>")
-    }
-    else {
-        title = event.title;
-        $('#delete_event').html("<a href = 'javascript:void(0);' onclick ='deleteEvent(" + event.id + ", " + false + ")'>Delete</a>");
-    }
-    $('#desc_dialog').dialog({
-      title: title,
-      modal: true,
-      width: 500,
-      close: function(event, ui){ $('#desc_dialog').dialog('destroy') }
-    });
+  $('#event_desc_dialog').html('')
+  $event_description  = $('<div />').html(event.description).attr("id", "event_description")
+  $event_actions      = $('<div />').attr("id", "event_actions")
+  $edit_event         = $('<span />').attr("id", "edit_event").html("<a href = 'javascript:void(0);' onclick ='editEvent(" + event.id + ")'>Edit</a>");
+  $delete_event       = $('<span />').attr("id", "delete_event")
+  if (event.recurring) {
+    title = event.title + "(Recurring)";
+    $delete_event.html("&nbsp; <a href = 'javascript:void(0);' onclick ='deleteEvent(" + event.id + ", " + false + ")'>Delete Only This Occurrence</a>");
+    $delete_event.append("&nbsp;&nbsp; <a href = 'javascript:void(0);' onclick ='deleteEvent(" + event.id + ", " + true + ")'>Delete All In Series</a>")
+    $delete_event.append("&nbsp;&nbsp; <a href = 'javascript:void(0);' onclick ='deleteEvent(" + event.id + ", \"future\")'>Delete All Future Events</a>")
+  } else {
+    title = event.title;
+    $delete_event.html("<a href = 'javascript:void(0);' onclick ='deleteEvent(" + event.id + ", " + false + ")'>Delete</a>");
+  }
+  $event_actions.append($edit_event).append($delete_event)
+  $('#event_desc_dialog').append($event_description).append($event_actions)
+  $('#event_desc_dialog').dialog({
+    title: title,
+    modal: true,
+    width: 500,
+    close: function(event, ui){ $('#event_desc_dialog').html(''); $('#event_desc_dialog').dialog('destroy') }
+  });
 }
 
 function editEvent(event_id){
-    jQuery.ajax({
-      url: "fullcalendar_engine/events/" + event_id + "/edit",
-      success: function(data) {
-        $('#event_desc').html(data['form']);
-      } 
-    });
+  jQuery.ajax({
+    url: "/fullcalendar_engine/events/" + event_id + "/edit",
+    success: function(data) {
+      $('#event_description').html(data['form']);
+    }
+  });
 }
 
 function deleteEvent(event_id, delete_all){
@@ -88,7 +102,7 @@ function deleteEvent(event_id, delete_all){
     data: 'authenticity_token=' + authenticity_token + '&delete_all=' + delete_all,
     dataType: 'script',
     type: 'delete',
-    url: "fullcalendar_engine/events/" + event_id,
+    url: "/fullcalendar_engine/events/" + event_id,
     success: refetch_events_and_close_dialog
   });
 }
@@ -100,29 +114,28 @@ function refetch_events_and_close_dialog() {
 
 function showPeriodAndFrequency(value){
   switch (value) {
-      case 'Daily':
-          $('#period').html('day');
-          $('#frequency').show();
-          break;
-      case 'Weekly':
-          $('#period').html('week');
-          $('#frequency').show();
-          break;
-      case 'Monthly':
-          $('#period').html('month');
-          $('#frequency').show();
-          break;
-      case 'Yearly':
-          $('#period').html('year');
-          $('#frequency').show();
-          break;
-          
-      default:
-          $('#frequency').hide();
+    case 'Daily':
+      $('#period').html('day');
+      $('#frequency').show();
+      break;
+    case 'Weekly':
+      $('#period').html('week');
+      $('#frequency').show();
+      break;
+    case 'Monthly':
+      $('#period').html('month');
+      $('#frequency').show();
+      break;
+    case 'Yearly':
+      $('#period').html('year');
+      $('#frequency').show();
+      break;
+    default:
+      $('#frequency').hide();
   }
 }
 $(document).ready(function(){
-  $('#create_event_dialog, #desc_dialog').on('submit', "#event_form", function(event) {
+  $('#create_event_dialog, #event_desc_dialog').on('submit', "#event_form", function(event) {
     var $spinner = $('.spinner');
     event.preventDefault();
     $.ajax({
